fa50979f3773c9748babc6f3dc0ed521e7bad0b6
Balavignesh J <balav3230@gmail.com>
2025-09-05 16:58:37 +0530

    serialize

--- api/views.py
+++ api/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+""" from django.shortcuts import render
+from django.http import JsonResponse """
+from students.models import Student
+from .serializers import StudentSerializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET'])
 def student_data(request):
-    data = {'id':1, 'name':"harish", "age":21}
-    return JsonResponse(data=data)
+    if request.method == 'GET':
+        student = Student.objects.all()
+        serializer = StudentSerializers(student, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- students/models.py
+++ students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=50)
+    student_dept = models.CharField(max_length=50)
+    student_age = models.IntegerField()
+
+    def __str__(self):
+        return self.student_name
